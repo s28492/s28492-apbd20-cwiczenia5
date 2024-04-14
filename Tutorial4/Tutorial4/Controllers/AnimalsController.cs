01cd6f4078d4ffcd2e084e7c89a4d0a6115dfeb3
Andrzzej <s28492@pjwstk.edu.pl>
2024-04-14 09:39:49 +0200

    Lab material added

---
+++ Tutorial4/Tutorial4/Controllers/AnimalsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tutorial4.Database;
+
+namespace Tutorial4.Controllers;
+
+[ApiController]
+[Route("/animals-controller")]
+// [Route("[controller]")]
+public class AnimalsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        var animals = new MockDb().Animals;
+        return Ok(animals);
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetAnimal(int id)
+    {
+        return Ok(id);
+    }
+    
+    [HttpPost]
+    public IActionResult AddAnimal()
+    {
+        return Created();
+    }
+}
---
+++ Tutorial4/Tutorial4/Endpoints/AnimalEndpoints.cs
@@ -0,0 +1,30 @@
+﻿using Tutorial4.Database;
+using Tutorial4.Models;
+
+namespace Tutorial4.Endpoints;
+
+public static class AnimalEndpoints
+{
+    public static void MapAnimalEndpoints(this WebApplication app)
+    {
+        app.MapGet("/animals", () =>
+        {
+            // 200 - Ok
+            // 201 - Created
+            // 400 - Bad Request
+            // 404 - Not Found
+            var animals = StaticData.animals;
+            return Results.Ok(animals);
+        });
+
+        app.MapGet("/animals/{id}", (int id) =>
+        {
+            return Results.Ok(id);
+        });
+
+        app.MapPost("/animals", (Animal animal) =>
+        {
+            return Results.Created("", animal);
+        });
+    }
+}
